eec5cd70051d9fb3b354c9c13a6e4551e7b7a916
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:56:26 -0500

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,7 @@
   const getTotalIsles = function (grid) {
 
         let n= grid.len
-        let m= len(grid[0])
+        let m= grid[0].len
         let ans= 0
         for(i=0 ; i<n; i++){
             for(j=0 ; j<m; j++){
