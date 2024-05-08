26af340396bf7376f523545b2bea5b48287c0c48
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:52:11 -0500

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,7 +4,7 @@
     n= len(grid)
         m= len(grid[0])
         ans= 0
-        for i in range(n):
+        for(i=0 ; i<n; i++) i in range(n):
             for j in range(m):
                 if grid[i][j]== 'L':
                     ans+=1
