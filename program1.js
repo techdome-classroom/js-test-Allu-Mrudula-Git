8ed0058531b28aceec71c5dc8b1a2d69561fca89
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:52:23 -0500

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,7 +4,7 @@
     n= len(grid)
         m= len(grid[0])
         ans= 0
-        for(i=0 ; i<n; i++) i in range(n):
+        for(i=0 ; i<n; i++){
             for j in range(m):
                 if grid[i][j]== 'L':
                     ans+=1
@@ -21,6 +21,7 @@
                             q.append((p[0], p[1]-1))
                         if(p[1]<m-1 and grid[p[0]][p[1]+1]=='L'):
                             q.append((p[0], p[1]+1))
+        }
         return ans
 
   };
