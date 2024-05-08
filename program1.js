69083f770072a1571344a509f052f778635d2bcb
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:51:53 -0500

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,27 @@
   const getTotalIsles = function (grid) {
 
 
-    
+    n= len(grid)
+        m= len(grid[0])
+        ans= 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]== 'L':
+                    ans+=1
+                    q= [(i,j)]
+                    while(len(q)>0):
+                        p= q[0]
+                        grid[p[0]][p[1]]= 'W'
+                        q.pop(0)
+                        if(p[0]>0 and grid[p[0]-1][p[1]]=='L'):
+                            q.append((p[0]-1, p[1]))
+                        if(p[0]<n-1 and grid[p[0]+1][p[1]]=='L'):
+                            q.append((p[0]+1, p[1]))
+                        if(p[1]>0 and grid[p[0]][p[1]-1]=='L'):
+                            q.append((p[0], p[1]-1))
+                        if(p[1]<m-1 and grid[p[0]][p[1]+1]=='L'):
+                            q.append((p[0], p[1]+1))
+        return ans
 
   };
   
